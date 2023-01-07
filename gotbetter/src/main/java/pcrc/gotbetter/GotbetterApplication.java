6939b98d9e339ace62f7b86cc9b844f9d193c0b4
jjinyeok <fun2314@naver.com>
2023-01-07 15:29:15 +0900

    [Setting]

---
+++ gotbetter/src/main/java/pcrc/gotbetter/GotbetterApplication.java
@@ -0,0 +1,13 @@
+package pcrc.gotbetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotbetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotbetterApplication.class, args);
+	}
+
+}
---
+++ gotbetter/src/test/java/pcrc/gotbetter/GotbetterApplicationTests.java
@@ -0,0 +1,13 @@
+package pcrc.gotbetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotbetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
