2c48c99b797991aaa993481628d35a60cc8747e7
coflskim <50487221+cofls980@users.noreply.github.com>
2023-02-06 21:35:46 +0900

    Revert "[fix] 로그아웃 전까지 로그인 유지(jwt)"

--- gotbetter/src/main/java/pcrc/gotbetter/user/data_access/repository/UserRepositoryQueryDSL.java
+++
@@ -1,6 +0,0 @@
-package pcrc.gotbetter.user.data_access.repository;
-
-public interface UserRepositoryQueryDSL {
-    Boolean existsByAuthidOrEmail(String auth_id, String email);
-    void updateRefreshToken(String auth_id, String refresh_token);
-}
