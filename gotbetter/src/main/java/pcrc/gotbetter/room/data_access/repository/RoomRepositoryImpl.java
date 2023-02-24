7c15799d16bdab3ccdd3d90231bb7b5bf0e95577
coflskim <qawse980@gmail.com>
2023-02-24 18:20:08 +0900

    [fix] use view table

--- gotbetter/src/main/java/pcrc/gotbetter/room/data_access/repository/RoomRepositoryImpl.java
+++ gotbetter/src/main/java/pcrc/gotbetter/room/data_access/repository/RoomRepositoryImpl.java
@@ -2,7 +2,6 @@ package pcrc.gotbetter.room.data_access.repository;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.util.StringUtils;
-import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,7 +9,6 @@ import pcrc.gotbetter.room.data_access.entity.Room;
 
 import java.util.List;
 
-import static pcrc.gotbetter.participant.data_access.entity.QParticipant.participant;
 import static pcrc.gotbetter.room.data_access.entity.QRoom.room;
 
 public class RoomRepositoryImpl implements RoomRepositoryQueryDSL{
@@ -20,28 +18,6 @@ public class RoomRepositoryImpl implements RoomRepositoryQueryDSL{
     @Autowired
     public RoomRepositoryImpl(JPAQueryFactory queryFactory) {
         this.queryFactory = queryFactory;
-    }
-
-    @Override
-    public List<Room> findUserRooms(Long user_id) {
-        return queryFactory
-                .select(room)
-                .from(room)
-                .leftJoin(participant)
-                .where(room.roomId.eq(participant.roomId), participantEqUserId(user_id))
-                .fetch();
-    }
-
-    @Override
-    public Room findRoomWithUserIdAndRoomId(Long user_id, Long room_id) {
-        return queryFactory
-                .select(room)
-                .from(room)
-                .where(room.roomId.eq(JPAExpressions
-                        .select(participant.roomId)
-                        .from(participant)
-                        .where(participantEqRoomId(room_id), participantEqUserId(user_id))))
-                .fetchFirst();
     }
 
     @Override
@@ -56,13 +32,13 @@ public class RoomRepositoryImpl implements RoomRepositoryQueryDSL{
     }
 
     @Override
-    public Boolean existByRoomCode(String room_code) {
-        Integer exists =  queryFactory
-                .selectOne()
-                .from(room)
-                .where(roomEqRoomCode(room_code))
-                .fetchFirst();
-        return exists != null;
+    @Transactional
+    public void updateCurrentWeek(Long room_id, Integer plusWeek) {
+        queryFactory
+                .update(room)
+                .set(room.currentWeek, room.currentWeek.add(plusWeek))
+                .where(roomEqRoomId(room_id))
+                .execute();
     }
 
     @Override
@@ -74,13 +50,13 @@ public class RoomRepositoryImpl implements RoomRepositoryQueryDSL{
     }
 
     @Override
-    @Transactional
-    public void updateCurrentWeek(Long room_id, Integer plusWeek) {
-        queryFactory
-                .update(room)
-                .set(room.currentWeek, room.currentWeek.add(plusWeek))
-                .where(roomEqRoomId(room_id))
-                .execute();
+    public Boolean existByRoomCode(String room_code) {
+        Integer exists =  queryFactory
+                .selectOne()
+                .from(room)
+                .where(roomEqRoomCode(room_code))
+                .fetchFirst();
+        return exists != null;
     }
 
     /**
@@ -98,21 +74,5 @@ public class RoomRepositoryImpl implements RoomRepositoryQueryDSL{
             return null;
         }
         return room.roomCode.eq(room_code);
-    }
-    /**
-     * participant eq
-     */
-    private BooleanExpression participantEqUserId(Long user_id) {
-        if (StringUtils.isNullOrEmpty(String.valueOf(user_id))) {
-            return null;
-        }
-        return participant.userId.eq(user_id);
-    }
-
-    private BooleanExpression participantEqRoomId(Long room_id) {
-        if (StringUtils.isNullOrEmpty(String.valueOf(room_id))) {
-            return null;
-        }
-        return participant.roomId.eq(room_id);
     }
 }
