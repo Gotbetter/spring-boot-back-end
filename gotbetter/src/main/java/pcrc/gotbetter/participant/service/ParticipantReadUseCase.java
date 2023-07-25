af52588b67ff22223fe95e0ede1ce29348ac9104
cofls980 <qawse980@gmail.com>
2023-07-25 16:57:09 +0900

    [refactor] profile 값에 이미지 바이트 스트링이 들어가도록 수정

--- gotbetter/src/main/java/pcrc/gotbetter/participant/service/ParticipantReadUseCase.java
+++ gotbetter/src/main/java/pcrc/gotbetter/participant/service/ParticipantReadUseCase.java
@@ -6,10 +6,11 @@ import lombok.ToString;
 import pcrc.gotbetter.participant.data_access.dto.JoinRequestDto;
 import pcrc.gotbetter.participant.data_access.dto.ParticipantDto;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface ParticipantReadUseCase {
-    List<FindParticipantResult> getMemberListInARoom(Long roomId, Boolean accepted);
+    List<FindParticipantResult> getMemberListInARoom(Long roomId, Boolean accepted) throws IOException;
     Integer getMyRefund(Long participantId);
 
     @Getter
@@ -24,27 +25,27 @@ public interface ParticipantReadUseCase {
         private final String profile;
         private final Boolean authority;
 
-        public static FindParticipantResult findByParticipant(ParticipantDto participantDto) {
+        public static FindParticipantResult findByParticipant(ParticipantDto participantDto, String profile) {
             return FindParticipantResult.builder()
                 .participantId(participantDto.getParticipantId())
                 .userId(participantDto.getUser().getUserId())
                 .authId(participantDto.getAuthId())
                 .username(participantDto.getUser().getUsername())
                 .email(participantDto.getUser().getEmail())
-                .profile(participantDto.getUser().getProfile())
+                .profile(profile)
                 .authority(participantDto.getAuthority())
                 .build();
         }
 
         public static FindParticipantResult findByParticipant(JoinRequestDto joinRequestResultDTO,
-            Long participantId, Boolean authority) {
+            Long participantId, Boolean authority, String profile) {
             return FindParticipantResult.builder()
                 .participantId(participantId)
                 .userId(joinRequestResultDTO.getUser().getUserId())
                 .authId(joinRequestResultDTO.getUserSet().getAuthId())
                 .username(joinRequestResultDTO.getUser().getUsername())
                 .email(joinRequestResultDTO.getUser().getEmail())
-                .profile(joinRequestResultDTO.getUser().getProfile())
+                .profile(profile)
                 .authority(authority)
                 .build();
         }
