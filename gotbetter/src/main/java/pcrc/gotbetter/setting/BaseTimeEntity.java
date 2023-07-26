8cccc17fd52b166a0a64d61ce30927f245df98c0
cofls980 <qawse980@gmail.com>
2023-07-26 22:26:36 +0900

    [feat] create_by_id와 updated_by_id 컬럼 추가

--- gotbetter/src/main/java/pcrc/gotbetter/setting/BaseTimeEntity.java
+++ gotbetter/src/main/java/pcrc/gotbetter/setting/BaseTimeEntity.java
@@ -1,40 +1,64 @@
 package pcrc.gotbetter.setting;
 
+import static pcrc.gotbetter.setting.security.SecurityUtil.*;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import lombok.Getter;
+
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@Getter
-@MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
 public abstract class BaseTimeEntity {
 
     @CreatedDate
-    @Column(updatable = false, nullable = false)
-    private LocalDateTime created_date;
+    @Column(name = "created_date", updatable = false, nullable = false)
+    private LocalDateTime createdDate;
 
     @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime updated_date;
+    @Column(name = "updated_date", nullable = false)
+    private LocalDateTime updatedDate;
+
+    @CreatedBy
+    @Column(name = "created_by_id", updatable = false, nullable = false)
+    private String createdById;
+
+    @LastModifiedBy
+    @Column(name = "updated_by_id")
+    private String updatedById;
 
     @PrePersist
     public void updateCreatedDate() {
-        this.created_date = LocalDateTime.now();
+        this.createdDate = LocalDateTime.now();
+        try {
+            this.createdById = getCurrentUserId().toString();
+        } catch (Exception e) {}
     }
 
     @PreUpdate
     public void updateUpdatedDate() {
-        this.updated_date = LocalDateTime.now();
+        this.updatedDate = LocalDateTime.now();
+        try {
+            this.updatedById = getCurrentUserId().toString();
+        } catch (Exception e) {}
     }
 
-    // post , pre
-    // created_by, updated_by
+    public void updateCreatedById(String userId) {
+        this.createdById = userId;
+    }
+
+    public void updateUpdatedById(String userId) {
+        this.updatedById = userId;
+    }
 }
