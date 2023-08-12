f82f68a839275b8a9d417e4b33c41e031f1888e2
coflskim <qawse980@gmail.com>
2023-08-12 15:03:51 +0900

    [feat] 관리자 권한 방 참여 요청자 추가 기능 구현

--- gotbetter/src/main/java/pcrc/gotbetter/participant/ui/requestBody/AdminParticipantJoinRequest.java
+++ gotbetter/src/main/java/pcrc/gotbetter/participant/ui/requestBody/AdminParticipantJoinRequest.java
@@ -1,6 +1,7 @@
 package pcrc.gotbetter.participant.ui.requestBody;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -8,7 +9,9 @@ import lombok.ToString;
 @Getter
 @ToString
 @NoArgsConstructor
-public class ParticipantJoinRequest {
+public class AdminParticipantJoinRequest {
+	@NotNull
+	private Long user_id;
 	@NotBlank
 	private String room_code;
 }
